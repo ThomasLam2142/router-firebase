6a9fec447635128b12c00348bae31135bfbf1b60
Thomas Lam <thomaslam2142@gmail.com>
2023-10-16 21:22:02 -0400

    Auth button redirects to dashboard if successful

--- src/GoogleAuthButton.tsx
+++ src/GoogleAuthButton.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { GoogleAuthProvider, signInWithPopup, Auth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 interface GoogleAuthButtonProps {
   auth: Auth;
 }
 
 const GoogleAuthButton: React.FC<GoogleAuthButtonProps> = ({ auth }) => {
+  let navigate = useNavigate();  
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
 
     try {
       await signInWithPopup(auth, provider);
-      // User has been signed in with Google
+        navigate('/dashboard');
     } catch (error) {
       // Handle authentication errors
       console.error('Google sign-in error:', error);

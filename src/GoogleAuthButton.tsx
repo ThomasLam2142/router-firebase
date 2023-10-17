6eb25dad27022a4170a3b445918d3b5df132772b
Thomas Lam <thomaslam2142@gmail.com>
2023-10-16 21:24:27 -0400

    added comments

--- src/GoogleAuthButton.tsx
+++ src/GoogleAuthButton.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { GoogleAuthProvider, signInWithPopup, Auth } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
+//Define TS interface
 interface GoogleAuthButtonProps {
   auth: Auth;
 }
 
+//Define GoogleAuthButton component
 const GoogleAuthButton: React.FC<GoogleAuthButtonProps> = ({ auth }) => {
+  //Routing object navigate
   let navigate = useNavigate();  
   const handleGoogleSignIn = async () => {
     const provider = new GoogleAuthProvider();
